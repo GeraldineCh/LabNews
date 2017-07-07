cf7a435d56d307995ee99316c706a23048c11dbe
Geraldine Chauca <gchaucae@gmail.com>
2017-07-07 14:35:46 -0500

    Barra superior + logo

---
+++ public/assets/js/bundle.js
@@ -0,0 +1,20 @@
+'use strict';
+const root = $('.root');
+const render = (root) => {
+	root.empty();
+	const wrapper = $('<div class="wrapper"></div>');
+    wrapper.append(Header());
+  
+	root.append(wrapper);
+}
+
+const update = function (){
+  render(root);
+};
+
+$( function() {
+	/*$.getJSON("./api/backup.json", function (result) {
+	});*/
+		const root = $('.root');
+		render(root);
+});
